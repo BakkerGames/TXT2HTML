da27a270f35180719ce27eb5985c7960863c9fe1
ELYSIAN\chime <scott@bakkergames.com>
2016-10-29 13:10:54 -0400

    Remove code signing for now

--- TXT2HTML/My Project/AssemblyInfo.vb
+++ TXT2HTML/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2016.2.3.0")> 
-<Assembly: AssemblyFileVersion("2016.2.3.0")> 
+<Assembly: AssemblyVersion("2016.8.11.213")> 
+<Assembly: AssemblyFileVersion("2016.8.11.213")> 
